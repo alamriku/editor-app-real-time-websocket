f4c16f7211477cdbd2b8b2e1899eee1e6324ac79
alamriku <alamriku069@gmail.com>
2021-10-01 23:23:17 +0600

    ace-build,ace-build-extension,redis for broadcast added

---
+++ resources/js/components/Editor.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="editor">
+        console.log('hellow world');
+    </div>
+</template>
+
+<script>
+    import ace from 'ace-builds';
+    import 'ace-builds/webpack-resolver';
+    import 'ace-builds/src-noconflict/mode-javascript';
+    export default {
+        mounted() {
+            const editor = ace.edit('editor');
+            const session = editor.getSession();
+            const doc = session.getDocument();
+
+            session.setMode('ace/mode/javascript');
+            editor.setFontSize('22px');
+        }
+    }
+</script>
+
+<style>
+    #editor {
+        height: 500px;
+    }
+</style>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <editor></editor>
+</div>
+@endsection
